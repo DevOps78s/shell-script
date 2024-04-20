5ae6b4829ed4fb6c7fccdc3c9a20541d674b61f1
DevOps78s <gshiva541@gmail.com>
2024-04-20 09:30:30 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I'm doing good. How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: Not Bad, I'm thinking to upgrade DevOps."
